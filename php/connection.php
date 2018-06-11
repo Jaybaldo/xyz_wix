30ea8cbdbb56e348a4477c43ca0b704715b3d0d2
JavierEQBM <javier@eqbm.co>
2018-06-11 11:29:26 -0500

    .

--- php/connection.php
+++ php/connection.php
@@ -1,6 +1,6 @@
 <?php
-	$host = "gator3171.hostgator.com";
-	$user = "eqbm_wixplor_usr";
-	$password = "%wixplor$$$$????_$";
-	$database = "eqbm_wixplor";
+	$host = "us-lh1.leasehost.com";
+	$user = "wixplorc_main";
+	$password = "0$;m_XQKEGZQ";
+	$database = "wixplorc_wixplor";
 ?>
